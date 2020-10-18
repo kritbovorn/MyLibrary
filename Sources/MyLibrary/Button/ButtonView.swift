5f5287ca33f7a9c03612a9cf50d458fa6f890404
kritbovorn <ktaweeyossak@yahoo.com>
2020-10-18 22:29:23 +0700

    Create ButtonView Test ; will Delete later

---
+++ Sources/MyLibrary/Button/ButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  ButtonView.swift
+//  
+//
+//  Created by Kritbovorn Taweeyossak on 18/10/2563 BE.
+//
+
+import SwiftUI
+
+public struct ButtonView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+public struct SwiftUIView_Previews: PreviewProvider {
+    public static var previews: some View {
+        ButtonView()
+    }
+}
